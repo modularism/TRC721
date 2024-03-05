03edd260c629e5b5828eb7582e82438b7f5ebcd6
Tao Paint System <162350356+TaoPaintSystem@users.noreply.github.com>
2024-03-05 19:22:52 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import bittensor as bt
+from bittensor.synapse import Synapse 
+
+class Deploy(bt.Synapse):
+
+class Mint(bt.Synapse):
+
+class Transfer(bt.Synapse):
+    
+class Approve(bt.Synapse):
+
+class BalanceOf(bt.Synapse):
+    
+class TotalSupply(bt.Synapse):
